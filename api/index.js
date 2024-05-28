263509509312c3d119cddefa1be16f1afd40ffb5
gobGrobw <albertfoeng12@gmail.com>
2024-05-28 16:04:25 +0700

    added link to cors

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
 	cors({
-		origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+		origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://blog-api-gamma-silk.vercel.app'],
 	})
 );
 
