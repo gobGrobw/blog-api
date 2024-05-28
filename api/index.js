29c7b367b8f98a75e777054db9373ecc0bead148
gobGrobw <albertfoeng12@gmail.com>
2024-05-28 15:31:52 +0700

    changed file name

---
+++ api/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+require('./middleware/passport.js');
+
+const app = express();
+
+const apiRouter = require('./router.js');
+
+// Establish connection with Database
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		console.log('Database connected!');
+	})
+	.catch((err) => {
+		console.error(err);
+	});
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+	cors({
+		origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+	})
+);
+
+// Setting request handler
+app.get('/', (req, res) => {
+	res.redirect('/api');
+});
+
+app.use('/api', apiRouter);
+
+// Turning server on
+app.listen(8080, () => {
+	console.log('Server online');
+});
+
